1cbb2077c22b81492e59d0b01667e1aa1c51a803
Judith <judiithgiralt@gmail.com>
2023-08-19 17:13:19 +0200

    Adding some styles

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,28 +1,39 @@
-import React, { useRef, useState } from "react"
+import React, { useRef, useState } from "react";
+
+import "./Homepage.css";
 
 const Homepage = () => {
-  // const [ newTodo, setNewTodo ] = useState<boolean>(false)
   const [ todoList, setTodoList ] = useState<string[]>([])
-  const todoRef = useRef(null)
+  const todoRef = useRef<any>(null)
 
+  const saveTodo = (event: any) => {
+    event.preventDefault();
 
-    const saveTodo = () => {
-      let list = todoList
-      console.log((todoRef.current as any).value)
-      list.push((todoRef.current as any).value)
-      setTodoList(list)
-      console.log(todoList)
-    }
+    let list = [...todoList]
+    const newTodo = todoRef.current.value
+    todoRef.current.value = ""
+    list.push(newTodo)
+    setTodoList(list);
+  }
 
-    return (
-    <div className="App">
-      <h1>
-        TODO List
-      </h1>
-      <input type="text" placeholder="Write a new Todo" ref={todoRef}></input>
-      <button onClick={() => saveTodo()} type="button">Save</button>
-      {todoList ? todoList.map((todo) => <p key={todo}>{todo}</p>) : null}
+  return (
+  <div className="App">
+    <h1>
+      TODO List
+    </h1>
+    <form onSubmit={saveTodo}>
+      <input type="text" placeholder="Write a new Todo" ref={todoRef} className="inputTodo"></input>
+      <button type="submit" className="buttonSubmit">Save</button>
+    </form>
+    <div className="todoList">
+      {todoList ? todoList.map((todo, i) => 
+        <div key={i} className="item">
+          <input type="checkbox" className="checkbox"/>
+          <span className="todoText">{todo}</span>
+        </div>
+        ) : null}
     </div>
-    )
+  </div>
+  )
 }
 export default Homepage
